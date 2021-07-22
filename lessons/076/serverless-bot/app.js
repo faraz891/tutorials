66b0336daaef49df0fb80ba9962097f09191d916
Anton Putra <me@antonputra.com>
2021-07-22 10:44:48 -0700

    tmp

---
+++ lessons/076/serverless-bot/app.js
@@ -0,0 +1,24 @@
+const { App } = require('@slack/bolt');
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET
+});
+
+app.message(async ({ message, say }) => {
+    console.log(message)
+    const reversedText = [...message.text].reverse().join("");
+    await say(reversedText);
+});
+
+
+app.event('app_mention', async ({ event, say }) => {
+    console.log(event)
+    await say("asd")
+});
+
+(async () => {
+  await app.start(process.env.PORT || 5000);
+  
+  console.log('Serverless Bolt is running!');
+})();

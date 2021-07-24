6e3e3d61a928945028d24e7bbc534e73b0543bf6
Anton Putra <me@antonputra.com>
2021-07-24 13:07:47 -0700

    tmp

--- lessons/076/wall-e/app.js
+++ lessons/076/wall-e/app.js
@@ -7,8 +7,8 @@ const signingSecret = process.env.SLACK_SIGNING_SECRET;
 const token = process.env.SLACK_BOT_TOKEN;
 
 exports.handler = (event, context, callback) => {
-    const body = JSON.parse(event.body);
     if (security.validateSlackRequest(event, signingSecret)) {
+        const body = JSON.parse(event.body);
         switch (body.type) {
             case "url_verification": callback(null, body.challenge); break;
             case "event_callback": processRequest(body, callback); break;
